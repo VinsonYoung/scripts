1b40bee06085de162a60927bc2157c72d86cc77f
VinsonYoung <43646618+VinsonYoung@users.noreply.github.com>
2025-08-23 12:03:22 +0800

    Update gen_ros_dpbr.sh

--- gen_ros_dpbr.sh
+++ gen_ros_dpbr.sh
@@ -5,22 +5,22 @@ mkdir -p ./pbr
 cd ./pbr
 
 # 电信
-wget --no-check-certificate -c -O ct.txt https://ispip.clang.cn/chinatelecom_cidr.txt
+wget --no-check-certificate -c -O ct.txt https://ispip.clang.cn/chinatelecom.txt
 grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}(\/[0-9]{1,2})?" ct.txt > ct.txt.tmp; mv ct.txt.tmp ct.txt
 # 联通
-wget --no-check-certificate -c -O cu.txt https://ispip.clang.cn/unicom_cnc_cidr.txt
+wget --no-check-certificate -c -O cu.txt https://ispip.clang.cn/unicom_cnc.txt
 grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}(\/[0-9]{1,2})?" cu.txt > cu.txt.tmp; mv cu.txt.tmp cu.txt
-# 移动
-wget --no-check-certificate -c -O cm.txt https://ispip.clang.cn/cmcc_cidr.txt
+# 移动(含铁通)
+wget --no-check-certificate -c -O cm.txt https://ispip.clang.cn/cmcc.txt
 grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}(\/[0-9]{1,2})?" cm.txt > cm.txt.tmp; mv cm.txt.tmp cm.txt
-# 铁通
-wget --no-check-certificate -c -O crtc.txt https://ispip.clang.cn/crtc_cidr.txt
-grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}(\/[0-9]{1,2})?" crtc.txt > crtc.txt.tmp; mv crtc.txt.tmp crtc.txt
+# 广电
+# wget --no-check-certificate -c -O chinabtn.txt https://ispip.clang.cn/chinabtn.txt
+# grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}(\/[0-9]{1,2})?" crtc.txt > crtc.txt.tmp; mv crtc.txt.tmp crtc.txt
 # 教育网
-wget --no-check-certificate -c -O cernet.txt https://ispip.clang.cn/cernet_cidr.txt
+wget --no-check-certificate -c -O cernet.txt https://ispip.clang.cn/cernet.txt
 grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}(\/[0-9]{1,2})?" cernet.txt > cernet.txt.tmp; mv cernet.txt.tmp cernet.txt
 # 长城宽带/鹏博士
-wget --no-check-certificate -c -O gwbn.txt https://ispip.clang.cn/gwbn_cidr.txt
+wget --no-check-certificate -c -O gwbn.txt https://ispip.clang.cn/gwbn.txt
 grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}(\/[0-9]{1,2})?" gwbn.txt > gwbn.txt.tmp; mv gwbn.txt.tmp gwbn.txt
 # 其他
 wget --no-check-certificate -c -O other.txt https://ispip.clang.cn/other.txt
@@ -42,8 +42,8 @@ for net in $(cat cm.txt) ; do
   echo "add list=dpbr-CMCC address=$net"
 done
 
-for net in $(cat crtc.txt) ; do
-  echo "add list=dpbr-CMCC address=$net"
+for net in $(cat chinabtn.txt) ; do
+  echo "add list=dpbr-CT address=$net"
 done
 
 for net in $(cat cernet.txt) ; do
